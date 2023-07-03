b1b27cfd556b18f53a1b640adc66c0a851dc7756
Jahongirhacking <Jahongirhacking@users.noreply.github.com>
2023-07-03 09:24:46 +0000

    deploy: 4606badc796e612549ebf2fb82e4adcb053363e3

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
+import typescript from "@rollup/plugin-typescript";
+import commonjs from "@rollup/plugin-commonjs";
+
+export default [
+  {
+    input: "src/app.ts",
+    output: { dir: "dist" },
+    plugins: [typescript(), nodeResolve({ browser: true }), terser()],
+  },
+  {
+    input: "src/comic.ts",
+    output: { dir: "dist" },
+    plugins: [
+      typescript(),
+      nodeResolve({ browser: true }),
+      terser(),
+      commonjs(),
+    ],
+  },
+];

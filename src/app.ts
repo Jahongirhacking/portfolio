bb77c5b749f6e3f2fe9ab9cd047bed2773252c07
Jahongirhacking <Jahongirhacking@users.noreply.github.com>
2024-01-22 21:10:35 +0000

    deploy: 92917c03105a6a2210ac3729baf4267dd76ac296

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,95 @@
 import { gsap } from "gsap";
 import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import langObject from "./langs";
+
+// Language
+const langVariants = {
+  langs: ["eng", "uzb", "deu"],
+  current: 0,
+};
+let currentLang: "eng" | "deu" | "uzb" = "eng";
+let currentLangObject = langObject[currentLang];
+
+const updateDocumentContent = () => {
+  // Nav
+  const navLink = Array.from(document.querySelectorAll(".nav__link"));
+  navLink[0].innerHTML =
+    currentLangObject.nav.latest + `<span class="badge new-badge">new</span>`;
+  navLink[1].textContent = currentLangObject.nav.about;
+  navLink[2].textContent = currentLangObject.nav.portfolio;
+  navLink[3].textContent = currentLangObject.nav.skills;
+  navLink[4].textContent = currentLangObject.nav.contact;
+  document
+    .querySelector(".nav__lang")!
+    .setAttribute("src", `./img/Languages/${currentLangObject.flag}`);
+  // Hero
+  const spans = Array.from(document.querySelectorAll(".header__info > span"));
+  spans[0].textContent = currentLangObject.hero.span1;
+  spans[1].textContent = currentLangObject.hero.span2;
+  document.querySelector(".header__my-name")!.textContent =
+    currentLangObject.hero.fullname;
+  document.querySelector(".header__my-job")!.textContent =
+    currentLangObject.hero.job;
+  // About
+  document.querySelector(".about_me__title")!.textContent =
+    currentLangObject.about.title;
+  document.querySelector(".about_me__info")!.textContent =
+    currentLangObject.about.info;
+  document.querySelector(".about_me__link")!.textContent =
+    currentLangObject.about.btn;
+  // Portfolio
+  document.querySelector(".portfolio__heading")!.textContent =
+    currentLangObject.portfolio.title;
+  const projects = Array.from(document.querySelectorAll(".portfolio__project"));
+  projects.forEach((project, index) => {
+    project.querySelector(".portfolio__title")!.textContent =
+      currentLangObject.portfolio.projects[index].name;
+    project.querySelector(".portfolio__info")!.textContent =
+      currentLangObject.portfolio.projects[index].description;
+  });
+  projects[projects.length - 1].querySelector(".demo-link")!.innerHTML =
+    currentLangObject.portfolio.projects[projects.length - 1].btn ||
+    "Click To See";
+  // Achievements
+  document.querySelector(".achievements__title")!.textContent =
+    currentLangObject.achievements.title;
+  const achievementsInfo = Array.from(
+    document.querySelectorAll(".achievements__card > .card__info")
+  );
+  achievementsInfo.forEach((achievementInfo, index) => {
+    achievementInfo.textContent = currentLangObject.achievements.cert[index];
+  });
+  // Skills
+  document.querySelector(".skills__title")!.textContent =
+    currentLangObject.skills.title;
+  document.querySelector(".skills__using__title")!.textContent =
+    currentLangObject.skills.now;
+  document.querySelector(".skills__learning__title")!.textContent =
+    currentLangObject.skills.learn;
+  document.querySelector(".skills__others__title")!.textContent =
+    currentLangObject.skills.other;
+  // Contact
+  document.querySelector(".footer__title")!.textContent =
+    currentLangObject.contact.title;
+  document.querySelector(".footer__made-heart")!.innerHTML =
+    currentLangObject.contact.made;
+  document.querySelector(".footer__small > span")!.textContent =
+    currentLangObject.contact.last;
+  document.getElementById("current__year")!.textContent = new Date()
+    .getFullYear()
+    .toString();
+};
+
+const changeLanguage = () => {
+  langVariants.current = (langVariants.current + 1) % langVariants.langs.length;
+  currentLang = langVariants.langs[langVariants.current] as
+    | "eng"
+    | "deu"
+    | "uzb";
+  currentLangObject = langObject[currentLang];
+  updateDocumentContent();
+};
+
 // ELEMENTS
 const navList = document.getElementById("nav__list") as HTMLUListElement;
 const overlayMenuBar = document.getElementById(
@@ -53,7 +143,7 @@ const fixElementsPosition = (): void => {
 const controlWindowSize = (): void => {
   fixElementsPosition();
   // Media Queries
-  if (window.innerWidth < 820) {
+  if (window.innerWidth < 900) {
     navList.classList.add("hidden");
     navList.classList.add("overlay");
     overlayMenuBar.classList.remove("fa-xmark");
@@ -87,16 +177,14 @@ function showOverlay(): void {
 const main = (): void => {
   const currentDate = new Date();
 
+  // Language
+  updateDocumentContent();
+  document
+    .querySelector(".nav__lang")!
+    .addEventListener("click", changeLanguage);
+
   // Modify Document
   controlWindowSize();
-  modifyTextContent(
-    document.getElementById("my-age") as HTMLSpanElement,
-    (currentDate.getFullYear() - 2004).toString()
-  );
-  modifyTextContent(
-    document.getElementById("current__year") as HTMLSpanElement,
-    currentDate.getFullYear().toString()
-  );
   //   EventListeners
   overlayMenuBar.addEventListener("click", showOverlay);
 
@@ -115,6 +203,7 @@ const main = (): void => {
 document.addEventListener("DOMContentLoaded", main);
 window.addEventListener("resize", controlWindowSize);
 document.addEventListener("keyup", controlKeyboard);
+
 if (leetcodeIcon.complete) fixElementsPosition();
 else leetcodeIcon.addEventListener("load", fixElementsPosition);
 
---
+++ src/langs.ts
@@ -0,0 +1,232 @@
+const langObject = {
+  eng: {
+    flag: "eng.jpg",
+    nav: {
+      latest: "Want Coffee?",
+      about: "About Me",
+      portfolio: "Portfolio",
+      skills: "Skills",
+      contact: "Contact Me",
+    },
+    hero: {
+      span1: "Hi, Welcome to my Portfolio",
+      span2: "My fullname is",
+      fullname: "Hayitov Jahongir Akrom ugli",
+      job: "I am a Web Developer and JavaScript Enthusiast",
+    },
+    about: {
+      title: "About Me",
+      info: `I am a ${
+        new Date().getFullYear() - 2004
+      } year old Web Developer. I am currently studying at the Tashkent Institute of Textile and Light Industry in Uzbekistan and in my spare time I am studying modern libraries and frameworks in this field of Web Development, and I have tried myself in several small projects so far. Innopolis University courses helped me to learn the basic concepts of "Software Design", "Data Structures and Algorithms", especially "Graph Theory". In addition, I try to solve problems of various difficulty on the Leetcode platform every day using my favorite programming languages JavaScript and C++. I think these skills of mine will help you!`,
+      btn: "MORE INFO",
+    },
+    portfolio: {
+      title: "PORTFOLIO",
+      projects: [
+        {
+          name: "Pokemono",
+          description: `"Pokemon" is a web application that I created in the RS School project. It allows users to view a list of Pokemon. Provides details such as Pokemon's name, picture, and other relevant information. It uses an architecture based on React components to efficiently manage and update the User Interface, providing a responsive and interactive experience for users interested in learning Pokemon information. Technologies used: React JS, TypeScript, Sass`,
+        },
+        {
+          name: "Todo List App",
+          description: `I used Binary Search Algorithm to make Reactive state manually in JavaScript by efficiently finding the index of the clicked element from the list of Todos, this enhanced the process of editing and deleting a list. Before deleting, it will show you a modal. I added "Clear All" button to clear everything and a new "read the whole text" function since the text content for list is limited`,
+        },
+        {
+          name: "Scivenia Beta Project",
+          description: `As part of this university software project, we created a specific functionality of a bigger web app. The purpose of this function is to visually represent the scientific collaboration between researchers and scientists. I used Breadth First Search algorithm in the client side to find all collaborators of the researcher. Used technologies: React JS and Sigma JS`,
+        },
+        {
+          name: "Travel Planner App",
+          description: `Travel Planner App - This project requires me to build out a travel app that, at a minimum, obtains a desired trip location & date from the user, and displays weather and an image of the location using information obtained from external APIs. I also added additional functionalities. Used technologies: HTML, SASS, JavaScript, Express JS and Webpack`,
+        },
+        {
+          name: "68th School App",
+          description: `68-DIMI WebApp - This application was created for students of our school in Uzbekistan. In this project, the students can play games, solve problems, take tests and compete to win. It has a leaderboard to show top students who collect points by taking tests and exams which being organized by the App. Used technologies: HTML, CSS, JavaScript and PHP`,
+        },
+        {
+          name: "Apple Layout",
+          description: `I was going to sharpen my CSS skills to work with Flexbox ang Grid Layouts. I choose Apple's website and tried to build its clone. I hope this layout looks like the real Apple Website. Used technologies: HTML and CSS`,
+        },
+        {
+          name: "Frontend Mentor Profile",
+          description: `You can see my other frontend related work. I think this platform is one of the best for learning and gaining experience, I have had a lot of good results through the many practices I have done on this platform.`,
+          btn: `Click To See <i class="fas fa-link"></i>`,
+        },
+      ],
+    },
+    achievements: {
+      title: "ACHIEVEMENTS",
+      cert: [
+        "Grant Offer from Innopolis University",
+        "Udacity Nanodegree of Front End Development",
+        "International Informatics Olympiad qualifying round",
+      ],
+    },
+    skills: {
+      title: "SKILLS",
+      now: "USING NOW",
+      learn: "LEARNING",
+      other: "OTHER SKILLS",
+    },
+    contact: {
+      title: "CONTACT ME",
+      made: `Made with <i class="fas fa-heart red-color"></i>`,
+      last: "last update:",
+    },
+  },
+  uzb: {
+    flag: "uzb.jpg",
+    nav: {
+      latest: "Kofe Bormi?",
+      about: "Men Haqimda",
+      portfolio: "Portfel",
+      skills: "Skillar",
+      contact: "Bog'lanish",
+    },
+    hero: {
+      span1: "Assalomu Alaykum! Portfelimga",
+      span2: "Xush Kelibsiz, mening ismim",
+      fullname: "Hayitov Jahongir Akrom o'g'li",
+      job: "Men JavaScriptga ishtiyoqmand Web Dasturchiman ",
+    },
+    about: {
+      title: "MEN HAQIMDA",
+      info: `Men ${
+        new Date().getFullYear() - 2004
+      } yoshli Web Dasturchiman. Hozirda O‘zbekistondagi Toshkent to‘qimachilik va yengil sanoat institutida tahsil olyapman va bo‘sh vaqtimda Web Development bo‘yicha ushbu sohadagi zamonaviy kutubxona va freymvorklarni o‘rganyapman, shuningdek, hozirgacha bir qancha kichik loyihalarda o'zimni sinab ko'rganman. Innopolis Universiteti kurslari menga "Dasturiy Ta'minotni Loyihalash", "Ma'lumotlar Tuzilmalari va Algoritmlar"ning asosiy tushunchalarini, ayniqsa "Graf Nazariyasi"ni o'rganishga yordam berdi. Bundan tashqari, men har kuni Leetcode platformasidagi turli qiyinchilikdagi masalalarni sevimli JavaScript va C++ dasturlash tillari yordamida yechishga harakat qilaman. O'ylashimcha, mening bu qobiliyatlarim sizga yordam beradi!`,
+      btn: "Ko'proq Bilish",
+    },
+    portfolio: {
+      title: "PORTFEL",
+      projects: [
+        {
+          name: "Pokemono",
+          description: `"Pokemono" bu men RS School loyihasida yaratgan veb-ilovadir. U foydalanuvchilarga Pokemonlar roʻyxatini koʻrish imkonini beradi. Pokemonlarning nomi, rasmi va boshqa tegishli maʼlumotlari kabi tafsilotlarni taqdim etadi. U Foydalanuvchi Interfeysini samarali boshqarish va yangilash uchun React komponentlariga asoslangan arxitekturadan foydalanadi, bu Pokemon ma'lumotlarini o'rganishga qiziqqan foydalanuvchilar uchun sezgir va interaktiv tajribani taqdim etadi. Amaldagi texnologiyalar: React JS, TypeScript, Sass`,
+        },
+        {
+          name: "Todo List App",
+          description: `Qilmoqchi bo'lgan ishlaringiz roʻyxatidan bosilgan element indeksini samarali topib, JavaScriptda qoʻlbola reaktiv state yaratish uchun "Ikkilik Qidiruv" algoritmidan foydalandim, bu roʻyxatni tahrirlash va oʻchirish jarayonini kuchaytirdi. O'chirishdan oldin u sizga modalni ko'rsatadi. Men hamma narsani tozalash uchun "Hammasini tozalash" tugmasini va yangi "butun matnni o'qish" funksiyasini qo'shdim, chunki ro'yxatdagi matn tarkibi cheklangan`,
+        },
+        {
+          name: "Scivenia Beta Loyihasi",
+          description: `Ushbu universitet dasturiy loyihasining bir qismi sifatida biz kattaroq veb-ilovaning o'ziga xos funksiyasini yaratdik. Ushbu funktsiyaning maqsadi tadqiqotchilar va olimlar o'rtasidagi ilmiy hamkorlikni vizual ko'rinishda namoyish qilishdir. Men tadqiqotchining barcha hamkorlarini topish uchun mijoz tomonida "Kenglik Bo'yicha Qidirish" algoritmidan foydalandim. Ishlatilgan texnologiyalar: React JS va Sigma JS`,
+        },
+        {
+          name: "Sayohatni Rejalashtiruvchi Sayt",
+          description: `Sayohatni rejalashtiruvchi Sayt - Bu loyiha mendan hech bo'lmaganda foydalanuvchidan kerakli sayohat joyi va sanasini oladigan hamda tashqi API-lardan olingan ma'lumotlardan foydalanib ob-havo va joyning tasvirini ko'rsatadigan sayohat ilovasini yaratishimni talab qildi. Men qo'shimcha funktsiyalarni ham qo'shdim. Ishlatilgan texnologiyalar: HTML, SASS, JavaScript, Express JS va Webpack`,
+        },
+        {
+          name: "68-DIMI App",
+          description: `68-DIMI WebApp - Ushbu ilova G'uzor tumanidagi 68-sonli ixtisoslashtirilgan maktabimiz o'quvchilari uchun yaratilgan. Ushbu loyihada o'quvchilar o'yin o'ynashlari, muammolarni hal qilishlari, testlarni yechishlari va g'alaba qozonish uchun kurashishlari mumkin. Ilova tomonidan tashkil etilgan test va imtihonlarni topshirish orqali ball to'plagan eng yaxshi talabalarni ko'rsatish uchun peshqadamlar jadvali mavjud. Ishlatilgan texnologiyalar: HTML, CSS, JavaScript va PHP`,
+        },
+        {
+          name: "Apple Maketi",
+          description: `Men Flexbox va Grid maketlar bilan ishlash uchun stillash ko'nikmalarimni oshirmoqchi edim. Men Apple veb-saytini tanladim va uning o'xshash klonini yaratishga harakat qildim. Umid qilamanki, bu maket haqiqiy Apple veb-saytiga o'xshadi. Ishlatilgan texnologiyalar: HTML va CSS`,
+        },
+        {
+          name: "Frontend Mentor Profilim",
+          description: `Frontend bilan bog'liq boshqa ishlarimni ko'rishingiz mumkin. O'ylaymanki, bu platforma o'rganish va tajriba orttirish uchun eng yaxshilaridan biri, men ushbu platformada qilgan ko'plab amaliyotlarim orqali juda ko'p yaxshi natijalarga erishdim.`,
+          btn: `Ko'rish Uchun Bosing <i class="fas fa-link"></i>`,
+        },
+      ],
+    },
+    achievements: {
+      title: "YUTUQLARIM",
+      cert: [
+        "Innopolis universitetidan grant taklifi",
+        "Front End Development bo'yicha Udacitydan Nanodaraja",
+        "Xalqaro informatika olimpiadasining Respublika saralash bosqichi",
+      ],
+    },
+    skills: {
+      title: "SKILLAR",
+      now: "HOZIR AMALIYOTDA",
+      learn: "O'RGANISHDA",
+      other: "BOSHQA SKILLAR",
+    },
+    contact: {
+      title: "BOG'LANISH",
+      made: `<i class="fas fa-heart red-color"></i> bilan qilingan!`,
+      last: "oxirgi yangilanish:",
+    },
+  },
+  deu: {
+    flag: "deu.jpg",
+    nav: {
+      latest: "Kaffee?",
+      about: "Über mich",
+      portfolio: "Portfolio",
+      skills: "Fähigkeiten",
+      contact: "Kontaktiere",
+    },
+    hero: {
+      span1: "Hallo, willkommen in meinem Portfolio",
+      span2: "Mein ganzer Name lautet",
+      fullname: "Hayitov Jahongir Akrom ugli",
+      job: "Ich bin ein Webentwickler mit einer Leidenschaft für JavaScript",
+    },
+    about: {
+      title: "Über mich",
+      info: `Ich bin ein ${
+        new Date().getFullYear() - 2004
+      }-jähriger Webentwickler. Ich studiere derzeit am Tashkent Institute of Textile and Light Industry in Usbekistan und beschäftige mich in meiner Freizeit mit modernen Bibliotheken und Frameworks in diesem Bereich der Webentwicklung und habe mich bisher in mehreren kleinen Projekten versucht. Die Kurse der Innopolis University haben mir geholfen, die Grundkonzepte von „Softwaredesign“, „Datenstrukturen und Algorithmen“ und insbesondere „Graphentheorie“ zu erlernen. Darüber hinaus versuche ich täglich, Probleme unterschiedlicher Schwierigkeitsgrade auf der Leetcode-Plattform zu lösen, indem ich meine Lieblingsprogrammiersprachen JavaScript und C++ verwende. Ich denke, diese meiner Fähigkeiten werden Ihnen helfen!`,
+      btn: "MEHR INFO",
+    },
+    portfolio: {
+      title: "PORTFOLIO",
+      projects: [
+        {
+          name: "Pokemono",
+          description: `„Pokemono“ ist eine Webanwendung, die ich im RS School-Projekt erstellt habe. Es ermöglicht Benutzern, eine Liste von Pokémon anzuzeigen. Bietet Details wie den Namen, das Bild und andere relevante Informationen des Pokémon. Es verwendet eine Architektur, die auf React-Komponenten basiert, um die Benutzeroberfläche effizient zu verwalten und zu aktualisieren und Benutzern, die Pokemon-Informationen lernen möchten, ein reaktionsfähiges und interaktives Erlebnis zu bieten. Verwendete Technologien: React JS, TypeScript, Sass`,
+        },
+        {
+          name: "Todo Listen App",
+          description: `Um den Index eines angeklickten Elements in Ihrer To-Do-Liste effektiv zu finden, habe ich den binären Suchalgorithmus verwendet, um einen Dummy-Reaktionszustand in JavaScript zu erstellen, der den Prozess des Bearbeitens und Löschens der Liste vereinfacht. Vor dem Löschen wird Ihnen das Modal angezeigt. Ich habe eine Schaltfläche „Alle löschen“ und eine neue Funktion „Gesamten Text lesen“ hinzugefügt, um alles zu löschen, da der Textinhalt in der Liste begrenzt ist`,
+        },
+        {
+          name: "Scivenia Beta-Projekt",
+          description: `Im Rahmen dieses universitären Softwareprojekts haben wir ein einzigartiges Feature einer größeren Webanwendung erstellt. Der Zweck dieser Funktion besteht darin, die wissenschaftliche Zusammenarbeit zwischen Forschern und Wissenschaftlern visuell darzustellen. Ich habe einen clientseitigen Latitude Search-Algorithmus verwendet, um alle Partner des Forschers zu finden. Verwendete Technologien: React JS und Sigma JS`,
+        },
+        {
+          name: "Reiseplaner-App",
+          description: `Reiseplaner-App – Für dieses Projekt muss ich eine Reise-App entwickeln, die zumindest den gewünschten Reiseort und das gewünschte Reisedatum vom Benutzer erhält und mithilfe von Informationen, die von externen APIs stammen, das Wetter und ein Bild des Ortes anzeigt. Ich habe auch zusätzliche Funktionalitäten hinzugefügt. Verwendete Technologien: HTML, SASS, JavaScript, Express JS und Webpack`,
+        },
+        {
+          name: "68. Schulapp",
+          description: `68-DIMI WebApp – Diese Anwendung wurde für Schüler unserer Fachschule Nr. 68 im Bezirk Guzor erstellt. In diesem Projekt können Schüler Spiele spielen, Probleme lösen, Tests lösen und um den Sieg kämpfen. Es gibt eine Bestenliste, auf der die besten Schüler angezeigt werden, die bei den von der App organisierten Tests und Prüfungen Punkte erzielt haben. Verwendete Technologien: HTML, CSS, JavaScript und PHP`,
+        },
+        {
+          name: "Apple-Layout",
+          description: `Ich wollte meine CSS-Kenntnisse verbessern, um mit Flexbox und Grid-Layouts arbeiten zu können. Ich habe die Website von Apple ausgewählt und versucht, einen Klon davon zu erstellen. Ich hoffe, dass dieses Layout wie die echte Apple-Website aussieht. Verwendete Technologien: HTML und CSS`,
+        },
+        {
+          name: "Frontend-Mentor-Profil",
+          description: `Sie können meine anderen Frontend-bezogenen Arbeiten sehen. Ich denke, dass diese Plattform eine der besten zum Lernen und Sammeln von Erfahrungen ist. Ich habe durch die vielen Übungen, die ich auf dieser Plattform durchgeführt habe, viele gute Ergebnisse erzielt.`,
+          btn: `Klicke um es zu sehen <i class="fas fa-link"></i>`,
+        },
+      ],
+    },
+    achievements: {
+      title: "ERFOLGE",
+      cert: [
+        "Stipendienangebot der Innopolis University",
+        "Udacity Nanodegree der Front-End-Entwicklung",
+        "Republikanische Qualifikationsphase der Internationalen Informatikolympiade",
+      ],
+    },
+    skills: {
+      title: "FÄHIGKEITEN",
+      now: "JETZT VERWENDEN",
+      learn: "LERNEN",
+      other: "ANDERE FÄHIGKEITEN",
+    },
+    contact: {
+      title: "KONTAKTIERE MICH",
+      made: `Gemacht mit <i class="fas fa-heart red-color"></i>`,
+      last: "letzte Aktualisierung:",
+    },
+  },
+};
+
+export default langObject;
